c8d695f1c3240bd240e74aa923fe78240229856a
Malte Kießling <mkalte666@googlemail.com>
2015-12-10 18:47:15 +0100

    ugh

--- source/lib/d2d/game/ui/uielement.d
+++ source/lib/d2d/game/ui/uielement.d
@@ -15,12 +15,20 @@ import d2d.game.ui.uievent;
     That means that if the "colored box 7" is at position [0.5,0.5] and is [0.5,0.5] in size and on that you put another box [0.0,0.0,0.5,0.5] on there, 
         its absolute coordinates are [0.5,0.5] and size [0.25,0.25]!
     This is very similar, but not eqal to, the Entities ParentRelative mode. The major difference is the -1..1 position space and the size.
+    To remember:
+        Child lays on top of parent
+        Absolute position ans size is relative to parents position and size
 */
 abstract class UIElement : Base
 {
     this() 
     {
         enableEventHandling();
+    }
+
+    ~this()
+    {
+        _unfocus();
     }
 
     /// The name of a ui object
@@ -33,10 +41,109 @@ abstract class UIElement : Base
         return _name=name;
     }
 
-    ///The position of a ui element
+    /// The position of a ui element
+    @property vec2 pos()
+    {
+        return _pos;
+    }
+    @property vec2 pos(vec2 p)
+    {
+        return _pos=p;
+    }
+
+    /// The size of a ui element
+    @property vec2 size()
+    {
+        return _size;
+    }
+    @property vec2 size(vec2 s)
+    {
+        return _size = s;
+    }
+
+    /// The absolute positoin of ui element - dependent from parent position and size
+    @property vec2 absolutePos()
+    {
+        if (this.parent && cast(UIElement)this.parent) {
+            auto p = cast(UIElement)this.parent;
+            return p.absolutePos + vec2(p.absoluteSize.x*_pos.x,p.absoluteSize.y*_pos.y);
+        }
+
+        return _pos;
+    }
+    
+    /// The absolute size of a ui element - depends on the parent objects size
+    @property vec2 absoluteSize()
+    {
+        if (this.parent && cast(UIElement)this.parent) {
+            auto p = cast(UIElement)this.parent;
+            return vec2(p.absoluteSize.x*_size.x,p.absoluteSize.y*_size.y);
+        }
+
+        return _size;
+    }
+
+    override void preUpdate()
+    {
+        
+    }
+
+    @property bool hovered()
+    {
+        return _hoverd;
+    }
+
+    @property bool clicked()
+    {
+        return _clicked;
+    }
+
+    @property bool focus()
+    {
+        return _focus;
+    }
+
     
 protected:
+    /// Sets the focus of a element and unfocuses the current one
+    void _setFocus() 
+    {
+        if (_focusedElement==this) {
+            return;
+        }
+        else if(_focusedElement) {
+            _focusedElement._unfocus();
+        }
+    }
 
+    /// Unfocuses this element if it is focused
+    void _unfocus()
+    {
+        if(_focus) {
+            _focus = false;
+            _focusedElement = null;
+        }
+    }
+
+    /// operates on relative postions. used to check if a ui element can be focused
+    /// Override to return false if you want to make an object that cant be focused
+    bool _focusable(vec2 pos) 
+    {
+        if(children.length==0) {
+            return true;
+        }
+
+        foreach(ref c; children) {
+            auto p = cast(UIElement)c;
+            if(p) {
+                if(p.pos.x <= pos.x && p.pos.y <= pos.y && p.pos.x+p.size.x>=p.pos.x && p.pos.y+p.size.y>=p.pos.y) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 private:
     /// name of a ui element
     string _name;
@@ -44,4 +151,17 @@ private:
     vec2 _pos;
     /// size of a ui element
     vec2 _size;
+    /// if true the element can be dragged around with the mouse
+    bool _dragable;
+
+    // The floowing are interaction statuses of the element
+    /// If the mouse is "hovering" above this object
+    bool _hoverd;
+    /// If the object is being clicked
+    bool _clicked;
+    /// If the object is in focus. Only one ui element can be in focus at a time. see also _focus
+    bool _focus;
+    /// If the object is being dragged around
+    bool _dragged;
+    static UIElement _focusedElement;
 }
--- source/lib/d2d/game/ui/uievent.d
+++ source/lib/d2d/game/ui/uievent.d
@@ -4,7 +4,24 @@
 module d2d.game.ui.uievent;
 
 import d2d.core.event;
+import d2d.game.ui.uielement;
 
 /**
     Base for all ui events
 */
+abstract class UiEvent : Event
+{
+    /// Creates a new UI event. Always attached to a ui element
+    this(UIElement element) 
+    {
+        _element = element;
+    }
+
+    final @property UIElement element()
+    {
+        return _element;
+    }
+private:
+    /// The UI element wich emmits this event
+    UIElement _element;
+}

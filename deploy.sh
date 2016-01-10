c1df2da9d24576a0eeff80f176ac32e348353650
mkalte <mkalte666@googlemail.com>
2016-01-10 17:18:08 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,6 @@ git clone "https://github.com/DregginGames/Dragon2D" --branch gh-pages pages
 rm -rf ./pages/docs
 cp -a ./docs ./pages/docs
 cd docs
+git add .
+git commit -m "Automated update of docs"
 git push --quiet "https://${GH_TOKEN}@github.com/DregginGames/Dragon2D" gh-pages > /dev/null 2>&1

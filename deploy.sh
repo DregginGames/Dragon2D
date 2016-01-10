979bf8d456248adaa44353b591341db43d7de850
mkalte <mkalte666@googlemail.com>
2016-01-10 18:10:16 +0100

    i want to yell at people but it works now
    also i realy should hve found a better way than this. but hey. its over for now

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git clone "https://github.com/DregginGames/Dragon2D" --branch gh-pages pages
 rm -rf ./pages/docs
 cp -a ./docs ./pages/docs
 cd pages
-git add -all .
+git add --all .
 git commit -m "Automated update of docs"
 git push --quiet "https://${GH_TOKEN}@github.com/DregginGames/Dragon2D" gh-pages > /dev/null 2>&1
